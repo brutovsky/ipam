7da62e56bed65f1ee154cc16f2f668e9326084da
DESKTOP-Q7R713D\Vadym <vadim.nakitnyak@gmail.com>
2021-04-25 14:22:49 +0300

    Added basic logout page.

--- ipam_project/ipam_project/urls.py
+++ ipam_project/ipam_project/urls.py
@@ -59,5 +59,6 @@ urlpatterns = [
     path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', user_views.register, name='register'),
 ]

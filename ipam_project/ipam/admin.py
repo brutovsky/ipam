21df00a097f4468c702fa939b90b91c935bea1bc
DESKTOP-Q7R713D\Vadym <vadim.nakitnyak@gmail.com>
2021-04-28 13:06:17 +0300

    Added ip module with IPAddress, IPPrefix and IPRole models.

--- ipam_project/ipam/admin.py
+++ ipam_project/ipam/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models.ip import *
 
 # Register your models here.
+
+admin.site.register(IPRole)
+admin.site.register(IPAddress)
+admin.site.register(IPPrefix)
--- ipam_project/ipam_project/settings.py
+++ ipam_project/ipam_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'users.apps.UsersConfig',
     'dcim.apps.DcimConfig',
+    'ipam.apps.IpamConfig',
 
     'crispy_forms',
 ]

fc27fc33363e61fceaf85852d80040e6f7d32907
DESKTOP-Q7R713D\Vadym <vadim.nakitnyak@gmail.com>
2021-05-11 12:57:01 +0300

    Added 'API' button to base.html Removed trailing slashes from token urls.

--- ipam_project/users/api/urls.py
+++ ipam_project/users/api/urls.py
@@ -16,8 +16,8 @@ urlpatterns = [
     path('account/change-password/', views.ChangePassword.as_view(), name='user_password-update'),
     path('groups/', views.GroupList.as_view(), name='group-list'),
     path('groups/<int:pk>/', views.GroupDetail.as_view(), name='group-detail'),
-    path('token/', views.MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token', views.MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
